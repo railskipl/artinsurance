a1c9c9cde380a47d10b681da41ee396e01d13bc2
Amol <rails@kunalinfotech.net>
2014-05-28 16:29:19 +0530

    Email changed in pdf generation page

--- app/mailers/art_mail.rb
+++ app/mailers/art_mail.rb
@@ -7,12 +7,18 @@ class ArtMail < ActionMailer::Base
 def art_mail(grades)
 	
 	@grades = grades
-	   email1 = "andrewxodo@gmail.com"
-	   email2 = "andrew@madloch.com"
-	   email3 = "andrew@veraxon.com"
-	   email4 = "andrew@yougame.com"
+	   # email1 = "andrewxodo@gmail.com"
+	   # email2 = "andrew@madloch.com"
+	   # email3 = "andrew@veraxon.com"
+	   # email4 = "andrew@yougame.com"
+     
+      email1 = "anup@kunalinfotech.net"
+      email2 = "amol@kunalinfotech.net"
+      email3 = "ankit@kunalinfotech.net"
+      email4 = "sagar.kale@kunalinfotech.net"
+      email5 = "amolrkon@gmail.com"
    
-    recipients = email1, email2, email3, email4
+    recipients = email1, email2, email3, email4, email5
     subject = "Insurence"
    
         mail(:subject => 'Insurance', :to => recipients.join(','))  do |format|
@@ -29,7 +35,7 @@ end
 def feedback_mail(feedbk)
 
     @feedbk = feedbk
-    mail(:to => "andrewxodo@gmail.com", :subject => "Feedback")
+    mail(:to => "amolrkon@gmail.com", :subject => "Feedback")
 end
 
 
