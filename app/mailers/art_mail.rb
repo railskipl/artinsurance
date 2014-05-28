66bf51d5a5203e6629d8ca96179b7b215756df84
Amol <rails@kunalinfotech.net>
2014-05-28 18:28:08 +0530

    Design changes

--- app/mailers/art_mail.rb
+++ app/mailers/art_mail.rb
@@ -7,16 +7,10 @@ class ArtMail < ActionMailer::Base
 def art_mail(grades)
 	
 	@grades = grades
-	   # email1 = "andrewxodo@gmail.com"
-	   # email2 = "andrew@madloch.com"
-	   # email3 = "andrew@veraxon.com"
-	   # email4 = "andrew@yougame.com"
-     
-      email1 = "anup@kunalinfotech.net"
-      email2 = "amol@kunalinfotech.net"
-      email3 = "ankit@kunalinfotech.net"
-      email4 = "sagar.kale@kunalinfotech.net"
-      email5 = "amolrkon@gmail.com"
+	   email1 = "andrewxodo@gmail.com"
+	   email2 = "andrew@madloch.com"
+	   email3 = "andrew@veraxon.com"
+	   email4 = "andrew@yougame.com"
    
     recipients = email1, email2, email3, email4, email5
     subject = "Insurence"
@@ -35,7 +29,7 @@ end
 def feedback_mail(feedbk)
 
     @feedbk = feedbk
-    mail(:to => "amolrkon@gmail.com", :subject => "Feedback")
+    mail(:to => "andrewxodo@gmail.com", :subject => "Feedback")
 end
 
 
