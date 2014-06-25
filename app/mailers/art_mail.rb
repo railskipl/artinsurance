95337c24b9386c0f6f3fa054da541529ffac390f
Amol <rails@kunalinfotech.net>
2014-06-25 15:22:50 +0530

    Pdf attachement in the mailer

--- app/mailers/art_mail.rb
+++ app/mailers/art_mail.rb
@@ -26,7 +26,7 @@ class ArtMail < ActionMailer::Base
 
     respond_to do |format|
       format.pdf do
-        attachments["Artist_coverage_binder.pdf"] = WickedPdf.new.pdf_from_string(render_to_string(:pdf => "receipt", :template => 'art_mail/art_mail.pdf.erb')
+        attachments["Artist_coverage_binder.pdf"] = WickedPdf.new.pdf_from_string(render_to_string(:pdf => "receipt", :template => 'art_mail/art_mail.pdf.erb'))
       end
     end
     
