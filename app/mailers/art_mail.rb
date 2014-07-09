7a3e0148117da58e0fc1b9c41480d2388e35b17d
railskipl <rails@kunalinfotech.net>
2014-07-09 18:43:00 +0530

    changes artist preview

--- app/mailers/art_mail.rb
+++ app/mailers/art_mail.rb
@@ -1,51 +1,47 @@
 class ArtMail < ActionMailer::Base
-  default from: "rubyrails9@gmail.com"
+   default from: "rubyrails9@gmail.com"
 
 
   def art_mail(grades, subscriber, cus_token)
    
-  	@grades = grades
-	   # email1 = "andrewxodo@gmail.com"
-	   # email2 = "andrew@madloch.com"
-	   # email3 = "andrew@veraxon.com"
-	   # email4 = "andrew@yougame.com"
-   #    email5 = subscriber
-   #    recipients = email5
-   #    @cus = cus_token
-	  # recipients = email1, email2, email3, email4, email5  
+    @grades = grades
+    @cus = cus_token
+    #  email1 = "andrewxodo@gmail.com"
+    #  email2 = "andrew@madloch.com"
+    #  email3 = "andrew@veraxon.com"
+    #  email4 = "andrew@yougame.com"
+    #  email5 = subscriber
+
+
+    # recipients = email1, email2, email3, email4, email5
+   
     email1 = "atishkumarlewate41@gmail.com"
     email2 = "sagar.kale@kunalinfotech.net"
     email3 = "amol@kunalinfotech.net"
     email4 = "amolrkon@gmail.com"
     email5 = subscriber
     recipients = email1, email2, email3, email4, email5
-   
+
     
     subject = "Insurence"
 
-
     
+    attachments["Artist_coverage_binder.pdf"] = WickedPdf.new.pdf_from_string(render_to_string(:pdf => "receipt", :template => 'art_mail/art_mail.pdf.erb'))
+    
+
     mail(:subject => 'Insurance', :to => recipients.join(',')) do |format|
       format.html
-      format.pdf do
-        attachments["Artist_coverage_binder.pdf"] = WickedPdf.new.pdf_from_string(
-        render_to_string(:pdf => "receipt", :template => 'art_mail/art_mail.pdf.erb')
-        )
-      end
-    end    
+    end
   end
 
   def check_mail(grades)
      @grades = grades
 
     if @grades["Studio_&_storage_of_art_are_in_a_basement"] == "Yes" && @grades["Is_there_history_of_back_up_drain"] == "Yes"
-
-       # email1 = "wfleischer@bfbond.com"
-       # email2 = "plouis@markelcorp.com"
-
+       email1 = "wfleischer@bfbond.com"
+       email2 = "plouis@markelcorp.com"
        # email1 = "atishkumarlewate41@gmail.com"
        # email2 = "sagar.kale@kunalinfotech.net"
-
        recipients = email1, email2
        subject = "Waiting For Approval"
 
@@ -56,11 +52,13 @@ class ArtMail < ActionMailer::Base
 
   def checkmail(grades)
      @grades = grades
+  
+    if @grades["Description Of Loss"].present? && @grades["Description Of Loss1"].present? && @grades["Description Of Loss2"].present?
+       email1 = "wfleischer@bfbond.com"
+       email2 = "plouis@markelcorp.com"
 
-    if @grades["Description Of Loss"] != nil && @grades["Description Of Loss1"] != nil && @grades["Description Of Loss2"] != nil
-       # email1 = "wfleischer@bfbond.com"
-       # email2 = "plouis@markelcorp.com"
-
+       # email1 = "atishkumarlewate41@gmail.com"
+       # email2 = "sagar.kale@kunalinfotech.net"
        recipients = email1, email2
        subject = "Waiting For Approval"
 
@@ -73,11 +71,10 @@ class ArtMail < ActionMailer::Base
      @grades = grades
 
     if @grades["Studio_&_storage_of_art_are_in_a_basement"] == "Yes" && @grades["Is_there_history_of_back_up_drain"] == "Yes"  && @grades["Description Of Loss"] != nil && @grades["Description Of Loss1"] != nil && @grades["Description Of Loss2"] != nil
-       # email1 = "wfleischer@bfbond.com"
-       # email2 = "plouis@markelcorp.com"
-        email1 = "atishkumarlewate41@gmail.com"
-        email2 = "kumaratish448@gmail.com"
-
+       email1 = "wfleischer@bfbond.com"
+       email2 = "plouis@markelcorp.com"
+       # email1 = "amol@kunalinfotech.net"
+       # email2 = "sagar.kale@kunalinfotech.net"
        recipients = email1, email2
        subject = "Waiting For Approval"
 
@@ -96,9 +93,7 @@ class ArtMail < ActionMailer::Base
        # email4 = "andrew@yougame.com"
        # email5 = subscriber
        # email6 = addemail
-     
-      recipients = email1, email2, email3, email4, email5, email6
-
+       # recipients = email1, email2, email3, email4, email5, email6
 
        email1 = "atishkumarlewate41@gmail.com"
        email2 = "sagar.kale@kunalinfotech.net"
@@ -122,6 +117,7 @@ class ArtMail < ActionMailer::Base
       mail(:to => "andrewxodo@gmail.com", :subject => "Feedback")
       #mail(:to => "atishkumarlewate41@gmail.com", :subject => "Feedback")
   end
+
 
 
 
